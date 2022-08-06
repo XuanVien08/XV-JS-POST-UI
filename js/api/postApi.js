85653a6d57a58de3c2162c1a7e139dfd1f072c20
Xuân Viển <xuanvienn123@gmail.com>
2022-08-07 00:00:01 +0700

    update api

---
+++ js/api/postApi.js
@@ -0,0 +1,25 @@
+import axiosClient from './axiosClient';
+const postApi = {
+  getAll(params) {
+    const url = '/posts';
+    return axiosClient.get(url, { params });
+  },
+  getById(id) {
+    const url = `/posts${id}`;
+    return axiosClient.get(url);
+  },
+  add(data) {
+    const url = '/posts';
+    return axiosClient.post(url, { data });
+  },
+  update(id) {
+    const url = `/posts${id}`;
+    return axiosClient.patch(url, { data });
+  },
+  remove() {
+    const url = `/posts${id}`;
+    return axiosClient.delete(url);
+  },
+};
+
+export default postApi;
